be92ef3eb7f48ba4ccaaf6af43812af922b869d5
iamatultiwwari <atult2386@gmail.com>
2026-06-18 11:53:00 +0530

    succesfully added server

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+
+dotenv.config();
+
+const app = express();
+
+//connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello There");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
